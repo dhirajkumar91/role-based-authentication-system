c7e90d8232fdc52258098ec15e3fa470c8c28e91
Dhiraj456186 <drai6186@gmail.com>
2026-04-03 01:21:28 +0530

    Updated API base URL to use environment variable for deployment

--- auth-frontend/src/services/authService.js
+++ auth-frontend/src/services/authService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/v1/auth";
+//const API_URL = "http://localhost:8080/api/v1/auth";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/v1/auth`;
 
 export const registerUser = async(userData) => {
     return axios.post(`${API_URL}/register`, userData);
